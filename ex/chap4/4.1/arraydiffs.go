3760680c7894773de89a87ee2e7aaa8e70bbaf34
marcusquigley <marcusquigley@gmail.com>
2026-01-23 08:25:43 -0800

    4.1

--- ex/chap4/4.1/arraydiffs.go
+++ ex/chap4/4.1/arraydiffs.go
@@ -2,16 +2,27 @@ package arraydiff
 
 import "crypto/sha256"
 
-// 149.4 ns/op             0 B/op          0 allocs/op
-func Diffs(a, b []byte) int {
-	var result int
-	ash := sha256.Sum256(a)
-	bsh := sha256.Sum256(b)
+var pc [256]byte
 
-	for i := range ash {
-		if ash[i] != bsh[i] {
-			result++
-		}
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func Diff(a, b []byte) int {
+	x := sha256.Sum256(a)
+	y := sha256.Sum256(b)
+	return BitsDiff(&x, &y)
+}
+
+// 149.4 ns/op             0 B/op          0 allocs/op
+// 161.3 ns/op             0 B/op          0 allocs/op
+func BitsDiff(a, b *[sha256.Size]byte) int {
+	var result int
+
+	for i := range sha256.Size {
+		result += int(pc[a[i]] ^ pc[b[i]])
 	}
 	return result
 }
--- ex/chap4/4.1/arraydiffs_test.go
+++ ex/chap4/4.1/arraydiffs_test.go
@@ -13,14 +13,14 @@ var testCases = []struct {
 		description: "x and X",
 		arg1:        []byte("x"),
 		arg2:        []byte("X"),
-		expected:    31,
+		expected:    115,
 	},
 }
 
 func TestDiffs(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.description, func(t *testing.T) {
-			got := Diffs(tc.arg1, tc.arg2)
+			got := Diff(tc.arg1, tc.arg2)
 			if got != tc.expected {
 				t.Errorf("got %d. wanted %d", got, tc.expected)
 			}
@@ -35,7 +35,7 @@ func BenchmarkDiffs(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		{
 			for _, tc := range testCases {
-				Diffs(tc.arg1, tc.arg2)
+				Diff(tc.arg1, tc.arg2)
 			}
 		}
 	}
